1929fb44caca074ef834259aa04739c62b8c63a1
Paige C <77869251+pkcody@users.noreply.github.com>
2021-03-19 00:21:29 -0700

    hopeful

--- KillerWave/Assets/Resources/Script/BasicEnemyRoatate.cs
+++ KillerWave/Assets/Resources/Script/BasicEnemyRoatate.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class BasicEnemyRoatate : MonoBehaviour
 {
     [SerializeField]
-    float speed = 200;
+    float speed = 0;
     void Update()
     {
         transform.Rotate(Vector3.left * Time.deltaTime * speed);
